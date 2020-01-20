a9b715c5042a63523b5d4fd03ce93a39e1dd7c0a
Eduardo <eduardo.esteves@original.io>
2020-01-19 23:24:56 -0300

    Updates

---
+++ precache-manifest.e00fdeb65133a1e88279f3685bafe993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9ed3dd38b3cb35362c3656f74100eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e47c41c058e52193195",
+    "url": "/static/css/main.0ca602d8.chunk.css"
+  },
+  {
+    "revision": "cc0963b143359ae4215d",
+    "url": "/static/js/2.d881b1b7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d881b1b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e47c41c058e52193195",
+    "url": "/static/js/main.a0c2bae4.chunk.js"
+  },
+  {
+    "revision": "e36cc1a78616bb9d40ea",
+    "url": "/static/js/runtime-main.9d863bc3.js"
+  }
+]);
